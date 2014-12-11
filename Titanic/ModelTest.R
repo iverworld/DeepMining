39a5ff7348b3a709e481795fc766975a03f935eb
iverworld <iverworld@yahoo.com.hk>
2014-12-12 00:25:53 +0800

    Tried a model.

--- Titanic/ModelTest.R
+++ Titanic/ModelTest.R
@@ -27,3 +27,12 @@ ada.tune <- train(Fate ~ Sex + Class + Age + Family + Embarked,
                   metric = "ROC",
                   tuneGrid = ada.grid,
                   trControl = cv.ctrl)
+
+rf.grid <- data.frame(.mtry = c(2, 3))
+set.seed(35)
+rf.tune <- train(Fate ~ Sex + Class + Age + Family + Embarked, 
+                 data = train.batch,
+                 method = "rf",
+                 metric = "ROC",
+                 tuneGrid = rf.grid,
+                 trControl = cv.ctrl)
